59e5c1d9e257bf42090223ad66017017e77ac6b9
Rodrigo Aguilar <rodrigoaguilar887@gmail.com>
2017-09-21 20:12:19 -0700

    center ontent

--- js/javascript.js
+++ js/javascript.js
@@ -1,34 +1,13 @@
 $(document).ready(function() {
-	centerMeContent();
-	centerAboutContent();
-	centerProjectsContent();
-	centerHireMeContent();
+	centerContent();
 });
 
-function centerMeContent() {
-	$content1 = $('.content1');
-	contentHeight = $content1.height();
-	containerHeight = $('.bg-1').height();
-	$content1.css("margin-top", (containerHeight/2 - contentHeight/2) + "px");
-}
-
-function centerAboutContent() {
-	$content2 = $('.content2');
-	contentHeight = $content2.height();
-	containerHeight = $('.bg-2').height();
-	$content2.css("margin-top", (containerHeight/2 - contentHeight/2) + "px");
-}
-
-function centerProjectsContent() {
-	$content3 = $('.content3');
-	contentHeight = $content3.height();
-	containerHeight = $('.bg-3').height();
-	$content3.css("margin-top", (containerHeight/2 - contentHeight/2) + "px");
-}
-
-function centerHireMeContent() {
-	$content4 = $('.content4');
-	contentHeight = $content4.height();
-	containerHeight = $('.bg-4').height();
-	$content4.css("margin-top", (containerHeight/2 - contentHeight/2) + "px");
+function centerContent() {
+	var NUMBEROFSECTIONS = 4;
+	for (var i = 1; i <= NUMBEROFSECTIONS; i++) {
+		$content = $(".content" + i);
+		contentHeight = $content.height();
+		containerHeight = $(".bg-" + i).height();
+		$content.css("margin-top", (containerHeight/2 - contentHeight/2) + "px");
+	}
 }
